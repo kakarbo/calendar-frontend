92f4857367dddcdd9b54659720621e9663e8ee10
Cristhian Carbonell <ccarbone@bancolombia.com.co>
2024-02-01 17:03:35 -0500

    configuraciones adcionales al calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,23 +1,12 @@
 
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-import enUS from 'date-fns/locale/en-US'
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
+import { addHours } from 'date-fns'
 
 import { Navbar } from '../'
+import { localizer, getMessagesES } from '../../helpers'
 
-const locales = {
-  'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
 
 const events = [{
   title: 'Cumpleaños del Jefe',
@@ -31,16 +20,36 @@ const events = [{
   }
 }]
 
+
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    console.log({event, start, end, isSelected})
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+
+    }
+  }
+
   return (
     <>
       <Navbar />
       <Calendar
+        culture='es'
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 80px )' }}
+        messages={ getMessagesES() }
+        eventPropGetter={ eventStyleGetter }
       />
     </>
   )
