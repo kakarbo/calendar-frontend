1d585c0aa4afbe63323c599f7ac2c182e08ea3c3
Cristhian Carbonell <ccarbone@bancolombia.com.co>
2024-03-26 20:00:12 -0500

    Realizara login de usuario

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
 import { CalendarApp } from './CalendarApp.jsx'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { store } from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CalendarApp />
+    <Provider store={ store }>
+      <CalendarApp />
+    </Provider>
   </React.StrictMode>,
 )
