9d887b7fde164e511035e5ca6e4318973a5dbfcd
Cristhian Carbonell <ccarbone@bancolombia.com.co>
2024-03-26 20:42:41 -0500

    Despachar acciones respectivas

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux"
 import { calendarApi } from "../api/"
 import { getEnvVariables } from "../helpers"
+import { clearErrorMessage, onChecking, onLogin, onLogout } from "../store"
 
 
 
@@ -12,16 +13,22 @@ export const useAuthStore = () => {
 
     const startLogin = async( { email, password } ) => {
 
-        console.log({ email, password})
-
-
+        dispatch( onChecking() )
         try {
 
-            const resp = calendarApi.post('/auth', { email, password })
-            console.log({ resp })
+            const { data } = await calendarApi.post('/auth', { email, password })
+            console.log(data)
+            
+            localStorage.setItem( 'token', data.token )
+            localStorage.setItem( 'token-init-date', new Date().getTime() )
+            dispatch( onLogin({ name: data.name, uid: data.uid}) )
 
         } catch (error) {
-            console.log( { error })
+            dispatch( onLogout('Credenciales incorrectas') )
+            setTimeout( () => {
+
+                dispatch( clearErrorMessage() )
+            }, 10 )
         }
     }
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { store } from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <Provider store={ store }>
       <CalendarApp />
     </Provider>
-  </React.StrictMode>,
+  </>,
 )
